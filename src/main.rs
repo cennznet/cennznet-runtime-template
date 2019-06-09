3f560a3cb981cebfaf72066436a751d741bc10f7
Bryan Chen <xlchen1291@gmail.com>
2019-06-10 10:23:12 +1200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,22 @@
 #![warn(unused_extern_crates)]
 
 mod chain_spec;
-mod service;
 mod cli;
+mod service;
 
-pub use substrate_cli::{VersionInfo, IntoExit, error};
+pub use substrate_cli::{error, IntoExit, VersionInfo};
 
 fn run() -> cli::error::Result<()> {
-	let version = VersionInfo {
-		name: "CENNZnet Runtime Template Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "cennznet-runtime-tempalte",
-		author: "Centrality Developers",
-		description: "CENNZnet Runtime Template",
-		support_url: "https://github.com/cennznet/cennznet-runtime-template/issues",
-	};
-	cli::run(::std::env::args(), cli::Exit, version)
+    let version = VersionInfo {
+        name: "CENNZnet Runtime Template Node",
+        commit: env!("VERGEN_SHA_SHORT"),
+        version: env!("CARGO_PKG_VERSION"),
+        executable_name: "cennznet-runtime-tempalte",
+        author: "Centrality Developers",
+        description: "CENNZnet Runtime Template",
+        support_url: "https://github.com/cennznet/cennznet-runtime-template/issues",
+    };
+    cli::run(::std::env::args(), cli::Exit, version)
 }
 
 error_chain::quick_main!(run);
-
